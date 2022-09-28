f6638777a39aa88175e51cdd9613224689d34b0c
dareekun <dareekun@live.com>
2022-09-28 16:15:22 +0000

    fixing mobile api

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -16,23 +16,25 @@ class MobileController extends Controller
                 $date =  bcrypt(rand(100, 999).date('ymdHis'));
                 $user_name = DB::table('users')->where('username', $request->username)->value('name');
                 DB::table('users')->where('username', $request->username)->update(['token_login' => $date]);
+                $department = DB::table('users')->leftJoin('department_list', 'users.department', '=', 'department_list.id')->where('users.username', $request->username)->value('department_list.department');
                 return response([
                     'status' => 200,
                     'message' => "Success!!!",
                     'token' => $date,
-                    'user' => $user_name
-                    ]);
+                    'user' => $user_name,
+                    'depart' => $department
+                ]);
             } else {
                 return response([
                     'status' => 500,
                     'message' => "Wrong Password"
-                    ]);
+                ]);
             }
         } else {
             return response([
                 'status' => 500,
                 'message' => "Account Not Found"
-                ]);
+            ]);
         }
     }
 
@@ -40,7 +42,7 @@ class MobileController extends Controller
         if (DB::table('users')->where('token_login', $request->token)->exists()) {
             $data = DB::table('production')->where('barcode', $request->id)->leftJoin('product', 'production.model_no', '=', 'product.id')
             ->leftJoin('quality', 'production.id', '=', 'quality.productionId')->leftJoin('users', 'quality.userId', '=', 'users.id')
-            ->select('product.model_no as model_no', 'production.shift as shift', 'production.lotno as lotno', 'production.parts_data as parts',
+            ->select('production.barcode as barcode', 'product.model_no as model_no', 'production.shift as shift', 'production.lotno as lotno', 'production.parts_data as parts',
             'production.date_1 as date_1', 'production.date_2 as date_2', 'production.name_1 as name_1', 'production.name_2 as name_2', 
             'production.fg_1 as finish_goods_1', 'production.fg_2 as finish_goods_2', 'production.ng_1 as no_goods_1', 'production.ng_2 as no_goods_2',
             'quality.judgement as judgement', 'users.name as checker_name')->get();
@@ -100,25 +102,25 @@ class MobileController extends Controller
                         'type' => "lotsize"
                     ]);
                 }
-                DB::table('production')->where('barcode', $request->id)->update([
-                    'fg_1'   => $request->lotsize,
-                    'fg_2'   => $request->totsbox,
-                    'status' => 1
-                ]);
                 $productionId = DB::table('production')->where('barcode', $request->id)->value('id');
                 $userId       = DB::table('users')->where('token_login',  $request->token)->value('id');
                 DB::table('quality')->insert([
                     'productionId' => $productionId,
                     'judgement'    => $request->judgeme,
-                    'remark'       => $request->remark,
+                    'remark'       => $request->remark != NULL ? : "-",
                     'userId'       => $userId,
+                ]);
+                DB::table('production')->where('barcode', $request->id)->update([
+                    'fg_1'   => $request->lotsize,
+                    'fg_2'   => $request->totsbox,
+                    'status' => 1
                 ]);
                 DB::table('product')->where('model_no', $request->model_no)->update([
                     'packing' => $request->pcs_box
                 ]);
                 $data = DB::table('production')->where('barcode', $request->id)->leftJoin('product', 'production.model_no', '=', 'product.id')
                 ->leftJoin('quality', 'production.id', '=', 'quality.productionId')->leftJoin('users', 'quality.userId', '=', 'users.id')
-                ->select('product.model_no as model_no', 'product.packing as packing', 'production.shift as shift', 'production.lotno as lotno', 'production.parts_data as parts',
+                ->select('production.barcode as barcode', 'product.model_no as model_no', 'product.packing as packing', 'production.shift as shift', 'production.lotno as lotno', 'production.parts_data as parts',
                 'production.date_1 as date_1', 'production.date_2 as date_2', 'production.name_1 as name_1', 'production.name_2 as name_2', 'quality.remark as remark',
                 'production.fg_1 as finish_goods_1', 'production.fg_2 as finish_goods_2', 'production.ng_1 as no_goods_1', 'production.ng_2 as no_goods_2',
                 'quality.judgement as judgement', 'users.name as checker_name', 'product.section as section', 'product.line as line')->get();
@@ -174,11 +176,13 @@ class MobileController extends Controller
             if (DB::table('production')->where('barcode', $request->id)->value('status') == 1) {
                 $data = DB::table('production')->where('barcode', $request->id)->leftJoin('product', 'production.model_no', '=', 'product.id')
                 ->leftJoin('quality', 'production.id', '=', 'quality.productionId')->leftJoin('users', 'quality.userId', '=', 'users.id')
-                ->select('product.model_no as model_no', 'production.shift as shift', 'production.lotno as lotno', 'production.parts_data as parts',
+                ->select('production.barcode as barcode', 'product.model_no as model_no', 'production.shift as shift', 'production.lotno as lotno', 'production.parts_data as parts',
                 'production.date_1 as date_1', 'production.date_2 as date_2', 'production.name_1 as name_1', 'production.name_2 as name_2', 
                 'production.fg_1 as finish_goods_1', 'production.fg_2 as finish_goods_2', 'production.ng_1 as no_goods_1', 'production.ng_2 as no_goods_2',
                 'quality.judgement as judgement', 'users.name as checker_name')->get();
+                $status = DB::table('production')->where('barcode', $request->id)->value('status');
                 return response([
+                    'post' => $status,
                     'data' => $data[0],
                     'status' => 200
                 ]);
@@ -196,15 +200,17 @@ class MobileController extends Controller
         }
     }
 
-    public function transfers_process_mobile(Request $request) {
+    public function processtransfers_mobile(Request $request) {
         if (DB::table('users')->where('token_login', $request->token)->where('department', 5)->exists()) {
             if (DB::table('production')->where('barcode', $request->id)->value('status') == 1) {
                 DB::table('production')->where('barcode', $request->id)->update([
                     'status' => 2
                 ]);
+                $productionId = DB::table('production')->where('barcode', $request->id)->value('id');
+                $userId = DB::table('users')->where('token_login',  $request->token)->value('id');
                 DB::table('transaction')->insert([
-                    'productionId'   => DB::table('production')->where('barcode', $request->id)->value('id'),
-                    'userId'         => DB::table('users')->where('token_login',  $request->token)->value('id'),
+                    'productionId'   => $productionId,
+                    'userId'         => $userId,
                     'referTransfers' => 0
                 ]);
                 return response([
@@ -225,12 +231,36 @@ class MobileController extends Controller
         }
     }
 
+    public function completetransaction_mobile() {
+        if (DB::table('users')->where('token_login', $request->token)->where('department', 5)->exists()) {
+                DB::table('production')->leftJoin('')->where('barcode', $request->id)->update([
+                    'status' => 3
+                ]);
+                $productionId = DB::table('production')->where('barcode', $request->id)->value('id');
+                $userId = DB::table('users')->where('token_login',  $request->token)->value('id');
+                DB::table('transaction')->insert([
+                    'productionId'   => $productionId,
+                    'userId'         => $userId,
+                    'referTransfers' => 0
+                ]);
+                return response([
+                    'data' => $data[0],
+                    'status' => 200
+                ]);
+        } else {            
+            return response([
+            'status' => 403,
+            'message' => "Forbiden Access, Session Not Exists"
+        ]);
+        }
+    }
+
     public function printlotcard_mobile(Request $request) {
         if (DB::table('users')->where('token_login', $request->token)->exists()) {
             if (DB::table('production')->where('barcode', $request->id)->value('status') >= 1) {
                 $customPaper = array(0,0,245,500);
                 $random = rand(10, 99);
-                $record = DB::table('production')->where('barcode', $id)->leftJoin('product', 'production.model_no', '=', 'product.id')
+                $record = DB::table('production')->where('barcode', $request->id)->leftJoin('product', 'production.model_no', '=', 'product.id')
                 ->select('production.id as id', 'production.barcode as barcode', 'product.model_no as model_no', 'production.lotno as lotno', 'production.shift as shift', 'production.parts_data as parts',
                 'production.fg_1 as fg_1', 'production.fg_2 as fg_2', 'production.ng_1 as ng_1', 'production.ng_2 as ng_2', 'production.date_1 as date_1', 'production.date_2 as date_2', 'production.status as status',
                 'production.name_1 as name_1', 'production.name_2 as name_2')
--- routes/api.php
+++ routes/api.php
@@ -31,10 +31,15 @@ Route::get('/reverse/{id}',  [ApiController::class, 'reverse']);
 
 Route::get('/all/products',  [ApiController::class, 'all_products'])->name('all_products');
 
-Route::post('/printinspection_mobile',  [MobileController::class, 'printinspection_mobile']);
+Route::post('/printlotcard_mobile',  [MobileController::class, 'printlotcard_mobile']);
+
 Route::post('/scaninspection_mobile',  [MobileController::class, 'scaninspection_mobile']);
 Route::post('/processinspection_mobile',  [MobileController::class, 'processinspection_mobile']);
 Route::post('/showinspection_mobile',  [MobileController::class, 'showinspection_mobile']);
+Route::post('/printinspection_mobile',  [MobileController::class, 'printinspection_mobile']);
+
+Route::post('/scantransfers_mobile',  [MobileController::class, 'scantransfers_mobile']);
+Route::post('/processtransfers_mobile',  [MobileController::class, 'processtransfers_mobile']);
 
 Route::post('/data/product',  [ApiController::class, 'data_product'])->name('data_product');
 Route::post('/data/user',  [ApiController::class, 'data_user'])->name('data_users');
